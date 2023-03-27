fdcdf76157d26f9d77cde18c23cddabd6f93b786
I Kit Cheng <matthewkit@gmail.com>
2023-03-27 01:30:59 +0100

    [UPDATE] removed while True in ask_ai() function because streamlit reruns the app after every widget interaction [FEATURE] Save uploaded files only if they don't already exist [FEATURE] Made two main functions, first one for loading relevat info, second one for asking the AI only if API key has been submitted. [FORMAT] Format code

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ import streamlit as st
 from util import bg_image
 import config
 import os
-from llama_index import SimpleDirectoryReader, GPTListIndex, readers, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+from llama_index import (
+    SimpleDirectoryReader,
+    GPTListIndex,
+    readers,
+    GPTSimpleVectorIndex,
+    LLMPredictor,
+    PromptHelper,
+)
 from langchain import OpenAI
-import sys
-import os
+
 
 def construct_index(directory_path):
     # set maximum input size
@@ -18,85 +24,116 @@ def construct_index(directory_path):
     chunk_size_limit = 600
 
     # define LLM
-    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.5, model_name="text-davinci-003", max_tokens=num_outputs))
-    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
- 
+    llm_predictor = LLMPredictor(
+        llm=OpenAI(
+            temperature=0.5, model_name="text-davinci-003", max_tokens=num_outputs
+        )
+    )
+    prompt_helper = PromptHelper(
+        max_input_size,
+        num_outputs,
+        max_chunk_overlap,
+        chunk_size_limit=chunk_size_limit,
+    )
+
     documents = SimpleDirectoryReader(directory_path).load_data()
-    
+
     index = GPTSimpleVectorIndex(
         documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper
     )
 
-    index.save_to_disk('/app/index.json')
+    index.save_to_disk("/app/index.json")
 
     return index
 
+
 def ask_ai():
-    index = GPTSimpleVectorIndex.load_from_disk('/app/index.json')
-    while True:
-        cols = st.columns(2)
-        query = cols[0].text_input("What do you want to ask? ")
+    st.write("Ask GPT-4-U your questions in the chat box below:")
+    index = GPTSimpleVectorIndex.load_from_disk("/app/index.json")
+    cols = st.columns(2)
+
+    query = cols[0].text_input("What do you want to ask? ")
+    if query:
         response = index.query(query, response_mode="compact")
-        st.write(f"Response: <b>{response.response}</b>")
-        
-        if st.button("Stop"): 
-            # delete the saved files
-            files = os.listdir('/app/context_data/')
-            for f in files:
-                os.remove(f)
+        cols[0].write(f"Response: <b>{response.response}</b>", unsafe_allow_html=True)
+
+        # if st.button("Stop"):
+        #     # delete the saved files
+        #     files = os.listdir('/app/context_data/')
+        #     for f in files:
+        #         os.remove(f)
+
+        return response
+
 
 def save_uploadedfile(uploadedfile, outpath):
-    with open(os.path.join(outpath, uploadedfile.name),"wb") as f:
+    with open(os.path.join(outpath, uploadedfile.name), "wb") as f:
         f.write(uploadedfile.getbuffer())
-    return st.success(f"Saved File: {uploadedfile.name} to {outpath}")
+
+    st.success(f"Saved File: {uploadedfile.name} to {outpath}")
+
 
 def main():
     st.title("GPT-4-U")
     cols = st.columns(2)
-    openai_api_key = cols[0].text_input("Enter your OpenAI API key here (New? Sign up [here](https://platform.openai.com/overview)):")
+    openai_api_key = cols[0].text_input(
+        "Enter your OpenAI API key here (New? Sign up [here](https://platform.openai.com/overview)):",
+        type="password",
+    )
 
     if cols[0].button("Submit"):
-        os.environ["OPENAI_API_KEY"] = openai_api_key
+        if openai_api_key:
+            os.environ["OPENAI_API_KEY"] = openai_api_key
+            st.success("Your API key has been saved!", icon="✅")
+        else:
+            st.error("Please enter your API key!", icon="❌")
 
-    uploaded_files = cols[0].file_uploader("Upload custom files (`.txt`, `.tex`, ...) or existing `index.json`",
-                                            type=None, accept_multiple_files=True)
+    uploaded_files = cols[0].file_uploader(
+        "Upload custom files (`.txt`, `.tex`, ...) or existing `index.json`",
+        type=None,
+        accept_multiple_files=True,
+    )
 
     if len(uploaded_files) > 0:
         for file in uploaded_files:
             # save custom files to `context_data`
-            if '.json' not in file.name: 
-                save_uploadedfile(file, outpath='/app/context_data/')
-            elif '.json' in file.name:
-                save_uploadedfile(file, outpath='/app/')
+            if ".json" not in file.name and not os.path.isfile(
+                f"/app/context_data/{file.name}"
+            ):
+                save_uploadedfile(file, outpath="/app/context_data/")
+            elif ".json" in file.name and not os.path.isfile("/app/index.json"):
+                save_uploadedfile(file, outpath="/app/")
 
-        
-        if not os.path.isfile('/app/index.json'): 
-            st.info('Creating `index.json`...', icon="ℹ️")
+        if not os.path.isfile("/app/index.json"):
+            st.info("Creating `index.json`...", icon="ℹ️")
             llamaindex = construct_index("/app/context_data/")
             st.success("Your data has been indexed successfully!", icon="✅")
             st.download_button(
                 label="Download index as .json",
-                data=file,
-                file_name='index.json',
-                mime='text/plain',
-                )
+                data=llamaindex,
+                file_name="index.json",
+                mime="text/plain",
+            )
 
-    if cols[0].button("Chat"):
-        main2()
+    return openai_api_key
 
-def main2():
-    st.write("Ask GPT-4-U your questions in the chat box below:")
-    ask_ai()
+
+def main2(openai_api_key):
+    if openai_api_key:
+        ask_ai()
+
 
 # setup page
-st.set_page_config(page_title="GPT4U", page_icon=":shark:", layout="wide",
-                initial_sidebar_state="expanded",
-                )
-
-# setup background image
-bg_image.set_png_as_page_bg(
-    f"{config.PATH_TO_IMAGES}/ChatGPTLogo.png"
+st.set_page_config(
+    page_title="GPT4U",
+    page_icon=":shark:",
+    layout="wide",
+    initial_sidebar_state="expanded",
 )
 
+# setup background image
+bg_image.set_png_as_page_bg(f"{config.PATH_TO_IMAGES}/ChatGPTLogo.png")
+
 if __name__ == "__main__":
-    main()
+    openai_api_key = main()
+    response = main2(openai_api_key)
